2230f8d38c3b737fff1e55b533f0117a20e7b5a2
ehsanghaffarii <ghafari.5000@gmail.com>
2022-06-05 15:45:53 +0430

    add shared types

---
+++ apps/avidtrader/pages/auth/signup/index.tsx
@@ -0,0 +1,14 @@
+import './index.module.scss';
+
+/* eslint-disable-next-line */
+export interface SignupProps {}
+
+export function Signup(props: SignupProps) {
+  return (
+    <div>
+      <h1>Welcome to Signup!</h1>
+    </div>
+  );
+}
+
+export default Signup;
